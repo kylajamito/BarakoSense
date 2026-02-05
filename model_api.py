e1956d0595d0a8b85431c9f95210e36c07e43027
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2026-02-05 19:09:26 +0800

    added __name__ comment in model_api.py for backup

--- model_api.py
+++ model_api.py
@@ -93,3 +93,8 @@ def predict():
 if __name__ == "__main__":
     print("🚀 BarakoSense running at http://127.0.0.1:5000")
     app.run(port=5000, debug=False)
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     print(f"Starting BarakoSense API on port {port}")
+#     app.run(host="0.0.0.0", port=port, debug=False)
